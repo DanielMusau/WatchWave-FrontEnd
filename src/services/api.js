0541c7b3cb428845da9b13ca0271f4af396cba21
DanielMusau <danielmusau.dm19@gmail.com>
2024-08-20 09:15:21 +0300

    New changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://watch-wave.azurewebsites.net/api';
+const API_URL = ' http://127.0.0.1:5000/api';
 
 const token = localStorage.getItem('jwtToken');
 
